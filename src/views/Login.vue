b5ee0ddcf5b10c35683b0a7c34c6f57b655ee63f
LouisNgatale <louisngatale@gmail.com>
2021-04-10 21:47:22 +0300

    Login form Ui v 0.0.2

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -16,39 +16,45 @@
         <img src="../assets/images/login_bg.png" alt="">
     </div>
 
-    <div class="col-md-4 login-container">
-      <div class="login-contents">
-        <div class="title"><p>Welcome Back!</p></div>
-        <div class="options">
-            <div class="login-option active">
-                <span>Login</span>
-            </div>
-            <div class="register-option">
-                <span>Register</span>
-                <span>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="13.381" height="8.642" viewBox="0 0 13.381 8.642">
-                      <path class="caret" id="caret" d="M5.98,19.087.291,13.4a1,1,0,0,1,0-1.418l.945-.945a1,1,0,0,1,1.418,0l4.032,4.032,4.032-4.032a1,1,0,0,1,1.418,0l.945.945a1,1,0,0,1,0,1.418L7.394,19.087a1,1,0,0,1-1.414,0Z" transform="translate(0.004 -10.74)" fill="#989898"/>
-                    </svg>
-                </span>
-            </div>
+<!--    Login Form      -->
+<div class="col-md-4 login-container">
+  <div class="login-contents">
+
+    <div class="title"><p>Welcome Back!</p></div>
+
+    <div class="options">
+        <div @click="loginForm" id="login-option" class="login-option active">
+            <span>Login</span>
         </div>
-        <div class="form">
-            <form action="">
-                <div class="form-floating mb-3">
-                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                    <label for="floatingInput">Email address</label>
-                </div>
-                <div class="form-floating">
-                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                    <label for="floatingPassword">Password</label>
-                </div>
-            </form>
+        <div @click="registerForm" id="register-option" class="register-option">
+            <span>Register</span>
+            <span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="13.381" height="8.642" viewBox="0 0 13.381 8.642">
+                  <path class="caret" id="caret" d="M5.98,19.087.291,13.4a1,1,0,0,1,0-1.418l.945-.945a1,1,0,0,1,1.418,0l4.032,4.032,4.032-4.032a1,1,0,0,1,1.418,0l.945.945a1,1,0,0,1,0,1.418L7.394,19.087a1,1,0,0,1-1.414,0Z" transform="translate(0.004 -10.74)" fill="#989898"/>
+                </svg>
+            </span>
         </div>
-        <div class="button">
-            <button class="btn btn-primary">Login</button>
-        </div>
-      </div>
     </div>
+
+    <div class="form">
+        <form action="">
+            <div class="form-floating mb-3">
+                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                <label for="floatingInput">Email address</label>
+            </div>
+            <div class="form-floating">
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                <label for="floatingPassword">Password</label>
+            </div>
+        </form>
+    </div>
+
+    <div class="button">
+        <button class="btn btn-primary">Login</button>
+    </div>
+
+  </div>
+</div>
 </div>
 
 </div>
@@ -56,7 +62,23 @@
 
 <script>
 export default {
-name: "Login"
+name: "Login",
+methods:{
+    loginForm:function (){
+        // eslint-disable-next-line no-undef
+      $("#login-option").addClass("active");
+
+        // eslint-disable-next-line no-undef
+      $("#register-option").removeClass("active");
+    },
+    registerForm:function (){
+        // eslint-disable-next-line no-undef
+      $("#register-option").addClass("active");
+
+        // eslint-disable-next-line no-undef
+      $("#login-option").removeClass("active");
+    }
+}
 }
 </script>
 
@@ -65,6 +87,7 @@ name: "Login"
     font-size: 18px;
     text-align: center;
     margin-bottom: 0;
+    margin-top: 15px;
     color: $primary;
 }
 .sub-heading{
@@ -107,9 +130,9 @@ name: "Login"
             cursor: pointer;
             width: 100px;
             padding: 3px 0;
-            &:hover{
+            /*&:hover{
                 border-bottom: 3px solid $primary;
-            }
+            }*/
         }
         .register-option{
             border-bottom: 3px solid $grey-200;
@@ -119,17 +142,19 @@ name: "Login"
             flex-direction: row;
             justify-content: space-between;
             width: 100px;
-            &:hover{
+            /*&:hover{
                 border-bottom: 3px solid $primary;
-            }
+            }*/
         }
         .active{
             border-bottom: 3px solid $primary;
             color: $primary;
             font-weight: bold;
+            transition: all .2s;
         }
         .active .caret{
             fill: $primary;
+            transition: all .2s;
         }
     }
 
@@ -141,6 +166,12 @@ name: "Login"
             border-radius: 0;
         }
         .form-floating label{
+            padding-left: 0;
+        }
+
+        input:focus + label{
+            color: $primary;
+            font-weight: 600;
             padding-left: 0;
         }
     }

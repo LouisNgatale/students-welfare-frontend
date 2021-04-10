fb09aacae81ab728e0edbca945f5bc09975d021d
LouisNgatale <louisngatale@gmail.com>
2021-04-10 15:33:42 +0300

    Student routes registration

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,65 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from "@/views/Login";
+import Register from "@/views/Register";
+import StudentHome from "@/views/student/Home";
+import Appeal from "@/views/student/academics/Appeal";
+import Postpone from "@/views/student/academics/Postpone";
+import Specials from "@/views/student/academics/Specials";
+import Request from "@/views/student/hostel/Request";
+import Advice from "@/views/student/welfare/Advice";
+import Suggestions from "@/views/student/welfare/Suggestions";
+import Rules from "@/views/student/welfare/Rules";
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/login',
+    name: 'Login',
+    component: Login
   },
   {
-    path: '/about',
-    name: 'About',
+    path:'/register',
+    name: 'Register',
+    component: Register
+  },
+  {
+    path: '/student',
+    children:[
+      {
+        path:'/home',
+        component:StudentHome
+      },
+      {
+        path:'/academics/appeal',
+        component: Appeal
+      },
+      {
+        path:'academics/postpone',
+        component: Postpone
+      },
+      {
+        path:'academics/specials',
+        component: Specials
+      },
+      {
+        path:'/student/hostel/request',
+        component: Request
+      },
+      {
+        path:'/student/welfare/advice',
+        component: Advice
+      },
+      {
+        path: '/student/welfare/suggestions',
+        component: Suggestions
+      },
+      {
+        path:'/student/welfare/rules',
+        component: Rules
+      }
+    ],
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,12 @@
 <template>
-<div></div>
+<div>
+  test
+</div>
 </template>
 
 <script>
 export default {
-name: "Appeal"
+name: "Login"
 }
 </script>
 

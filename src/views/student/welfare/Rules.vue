a7c2dc96842c5d220a329d517635f16b8dbb1076
LouisNgatale <louisngatale@gmail.com>
2021-04-18 19:33:21 +0300

    Rules display

--- src/views/student/welfare/Rules.vue
+++ src/views/student/welfare/Rules.vue
@@ -18,12 +18,46 @@
             </div>
         </div>
     </div>
+
+    <div class="container-fluid">
+        <div v-for="rule in rules" :key="rule.id" class="row">
+            <div class="col">
+                <div class="my-2 rule-container">
+                    <div class="title">
+                        {{ rule.title }}
+                    </div>
+                    <div class="body">
+                        <p>{{ rule.description }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: "Welfare"
+  name: "Welfare",
+    data(){
+      return{
+          rules:[]
+      }
+    },
+    created() {
+      axios.get("http://localhost:8085/api/welfare/rules/all")
+        .then(response => {
+            this.rules = response.data.suggestionsResponses.map(item => {
+                return item;
+            })
+            console.log(this.rules)
+        })
+        .catch(errorMessage => {
+            console.log(errorMessage);
+        })
+    }
 }
 </script>
 
@@ -39,6 +73,17 @@ export default {
 .top-header{
     display: flex;
     justify-content: space-between;
-
+}
+.rule-container{
+    background: white;
+    width: 100%;
+    min-height: auto;
+    padding: 5px 10px;
+    .title{
+        font-weight: bold;
+    }
+    .body{
+        font-size: 14px;
+    }
 }
 </style>

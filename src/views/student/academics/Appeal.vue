85832fd68f26845562873d780d3b2811d7cf617c
LouisNgatale <louisngatale@gmail.com>
2021-04-22 21:19:26 +0300

    Appeal form

--- src/views/student/academics/Appeal.vue
+++ src/views/student/academics/Appeal.vue
@@ -11,7 +11,7 @@
         <div class="col ">
             <div class="top-header my-2">
                 <span>Appeals</span>
-                <button class="add_appeal">
+                <button  @click="modalOpen = true"  class="add_appeal">
                     <img src="../../../assets/icons/plus.svg" alt="">
                 </button>
             </div>
@@ -98,12 +98,101 @@
         </div>
     </div>
 
+    <teleport to="modal">
+        <div v-if="modalOpen" class="modal">
+            <div class="new-suggestion">
+                <p>Create appeal request</p>
+                <div class="mb-3 input">
+                    <input v-model="formData.fullName" type="text" class="form-control" autocomplete="off" id="name" placeholder="Full Name">
+                </div>
+                <div class="mb-3 input">
+                    <input v-model="formData.registrationNumber" type="text" class="form-control" autocomplete="off" id="regNo" placeholder="Registration Number">
+                </div>
+                <select class="form-select mb-3 input" aria-label="Default select example" v-model="formData.department">
+                    <option value="" selected disabled>Department</option>
+                    <option v-for="department in departments" :value="department" v-bind:key="department">
+                        {{ department }}
+                    </option>
+                </select>
+                <select class="form-select mb-3 input" aria-label="Default select example" v-model="formData.semester">
+                    <option value="" selected disabled>Semester</option>
+                    <option v-for="semester in semesters" :value="semester" v-bind:key="semester">
+                        {{ semester }}
+                    </option>
+                </select>
+                <div class="mb-3 input">
+                    <input v-model="name" type="text" class="form-control" autocomplete="off" id="year" placeholder="Subject">
+                </div>
+
+                <div class="mb-3 input">
+                    <textarea v-model="formData.reason" class="form-control" placeholder="Reason" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+                <div class="mb-3">
+                    <label for="formFileMultiple" class="form-label">Attachments</label>
+                    <input class="form-control" type="file" id="formFileMultiple" multiple>
+                </div>
+                <div v-if="error" class="container-fluid error mb-2">
+                    <div class="row m">
+                        <div class="col pl-0">
+                            {{ error }}
+                        </div>
+                    </div>
+                </div>
+                <div v-if="message" class="container-fluid success mb-2">
+                    <div class="row m">
+                        <div class="col pl-0">
+                            {{ message }}
+                        </div>
+                    </div>
+                </div>
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col pl-0">
+                            <button :disabled="loading" class="btn btn-primary mr-2" @click="submit">
+                                <span v-show="loading" class="spinner-border spinner-border-sm mr-2"></span>
+                                Submit
+                            </button>
+                            <button class="btn btn-danger" @click="modalOpen = false">
+                                Close
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </teleport>
+
 </div>
 </template>
 
 <script>
 export default {
-name: "Appeal"
+name: "Appeal",
+    data(){
+        return{
+            modalOpen: false,
+            departments:[
+                "COMPUTER STUDIES",
+                "CIVIL ENGINEERING",
+                "LAB TECH",
+                "TELECOMMUNICATION ENGINEERING",
+                "ELECTRICAL ENGINEERING",
+                "MECHANICAL ENGINEERING",
+            ],
+            semesters:["ONE","TWO","BOTH"],
+            formData:{
+                fullName:"",
+                registrationNumber:"",
+                year:"",
+                semester:"",
+                reason:"",
+                department:""
+            },
+            message:"",
+            error:"",
+            loading:false
+        }
+    }
 }
 </script>
 
@@ -155,5 +244,39 @@ name: "Appeal"
         background: $grey-300;
     }
 
+}
+
+.modal {
+    position: absolute;
+    top: 0; right: 0; bottom: 0; left: 0;
+    background-color: rgba(0,0,0,.5);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+.modal > div {
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    justify-content: center;
+    background-color: white;
+    width: 400px;
+    min-height: 300px;
+    padding: 10px;
+}
+.new-suggestion{
+    .input{
+        width: 100%;
+    }
+}
+.error{
+    font-size: 14px;
+    color: $danger;
+}
+.success{
+    font-size: 14px;
+    color: $success;
 }
 </style>

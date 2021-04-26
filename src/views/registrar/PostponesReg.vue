553916a828e055097d962556dc07a4444e57af6c
LouisNgatale <louisngatale@gmail.com>
2021-04-26 17:35:01 +0300

    Postpones

--- src/views/registrar/PostponesReg.vue
+++ src/views/registrar/PostponesReg.vue
@@ -9,33 +9,67 @@
 
 
         <div class="row">
-            <div class="col">
-                <table class="table">
+        <div class="col">
+            <table class="table">
+                <tr class="table-heading">
+                    <th>S/N</th>
+                    <th>Student</th>
+                    <th>ID</th>
+                    <th>Period</th>
+                    <th>Actions</th>
+                </tr>
+                <tr v-for="(result,index) in results" :key="index" class="td">
+                    <td class="sn">{{index+1}}</td>
+                    <td>{{result.fullname}}</td>
+                    <td>{{result.studentId}}</td>
+                    <td>{{result.period}}</td>
+                    <td>
+                        <select class="form-select input" v-model="result.status" @change="changed(result)" aria-label="Default select example">
+                            <option value="Pending" selected disabled>Action</option>
+                                <option value="Approved">Approve</option>
+                                <option value="Denied"  >Deny</option>
+                        </select>
+                    </td>
+                </tr>
 
-                    <tr class="table-heading">
-                        <th>S/N</th>
-                        <th>Student</th>
-                        <th>ID</th>
-                        <th>Semester</th>
-                        <th>Actions</th>
-                    </tr>
-
-                    <tr class="td">
-                        <td class="sn">2</td>
-                        <td>Name</td>
-                        <td>ID</td>
-                        <td>Subject</td>
-                        <td>
-                            <select class="form-select input" aria-label="Default select example" v-model="formData.semester">
-                                <option value="" selected >Approve</option>
-                                <option value="" selected >Deny</option>
-                            </select>
-                        </td>
-                    </tr>
-
-                </table>
-            </div>
+            </table>
         </div>
+    </div>
+    <teleport name="modal" to="modal">
+            <div v-if="modalOpen" class="modal">
+                <div class="new-suggestion">
+                        <p>Confirm</p>
+                        <div v-if="message" class="container-fluid success mb-2">
+                            <div class="row m">
+                                <div class="col pl-0">
+                                    {{ message }}
+                                </div>
+                            </div>
+                        </div>
+                        <div v-if="error" class="container-fluid error mb-2">
+                            <div class="row m">
+                                <div class="col pl-0">
+                                    {{ error }}
+                                </div>
+                            </div>
+                        </div>
+                        <div class="container-fluid">
+                            <div class="row jus">
+                                <div class="col pl-0">
+                                    <button :disabled="loading" class="btn btn-primary mr-2" @click="proceed">
+                                        <span v-show="loading" class="spinner-border spinner-border-sm mr-2"></span>
+                                        Proceed
+                                    </button>
+                                    <button class="btn btn-danger" @click="modalOpen = false">
+                                        Close
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </teleport>
+
 
 
     </div>
@@ -47,80 +81,62 @@ import axios from "axios";
 export default {
     name: "PostponesReg",
     data(){
-        return{
-            modalOpen: false,
-            departments:[
-                "COMPUTER STUDIES",
-                "CIVIL ENGINEERING",
-                "LAB TECH",
-                "TELECOMMUNICATION ENGINEERING",
-                "ELECTRICAL ENGINEERING",
-                "MECHANICAL ENGINEERING",
-            ],
-            semesters:["ONE","TWO","BOTH"],
-            results:[],
-            formData:{
-                fullName:"",
-                registrationNumber:"",
-                year:"",
-                semester:"",
-                course:"",
-                subject:"",
-                reason:"",
-                department:""
-            },
-            message:"",
-            error:"",
-            loading:false
+    return{
+        results:[],
+        action:"",
+        currentStatus:"",
+        modalOpen: false,
+        loading:false,
+        id:"",
+        message:"",
+        error:""
+    }
+},
+methods:{
+    changed:function(e){
+        this.modalOpen=true
+        this.message=""
+        this.error = "";
+        this.currentStatus = e.status
+        console.log(e.status);
+        this.id = e.id
+    },
+    proceed:function(){
+        if(this.currentStatus === "Approved"){
+            axios.put(`http://localhost:8086/api/academics/postpones/registrar/${this.id}/approve`)
+            .then(() => {
+                this.message="Successfully updated!"
+                this.error = "";
+
+            })
+            .catch(() => {
+                this.error = "Couldn't update!";
+                this.message = "";
+            })
+        }else{
+            axios.put(`http://localhost:8086/api/academics/postpones/registrar/${this.id}/deny`)
+            .then(() => {
+                this.message="Successfully updated!"
+                this.error = "";
+            })
+            .catch(() => {
+                this.error = "Couldn't update!";
+                this.message = "";
+            })
         }
     },
-    methods:{
-        submit(){
-            this.loading = true;
-            if (this.formData.fullName !== ""
-                && this.formData.registrationNumber !== ""
-                && this.formData.department !== ""
-                && this.formData.course !== ""
-                && this.formData.semester !== ""){
-                axios.post("http://localhost:8086/api/academics/appeals/create",{
-                    fullName:this.formData.fullName,
-                    registrationNumber:this.formData.registrationNumber,
-                    department:this.formData.department,
-                    course:this.formData.course,
-                    semester:this.formData.semester,
-                    subject:this.formData.subject,
-                    reason:this.formData.reason,
-                }).then(response => {
-                    this.loading = false;
-                    this.error = "";
-                    this.message = "Appeal created successfully";
-                    console.log(response.data)
-                }).catch(errorMessage => {
-                    this.loading = false;
-                    this.message = "";
-                    this.error = "Couldn't create appeal!";
-                    console.log(errorMessage);
+    getPostpones:function(){
+        axios.get("http://localhost:8086/api/academics/postpones/registrar/get/all")
+        .then(Response => {
+            this.results = Response.data.postpones.map(item => {
+                    return item;
                 })
-            }else {
-                this.loading = false;
-                this.error = "Please fill the form!";
-            }
-        },
-        getSpecials(){
-            axios.get("http://localhost:8086/api/academics/specials/student/get")
-                .then(response => {
-                    this.results = []
-                    response.data.specials.forEach(item =>{
-                        this.results.push(item);
-                    })
-                }).catch(errorMessage => {
-                console.log(errorMessage)
-            });
-        },
-    },
-    created() {
-        this.getSpecials()
+        })
     }
+},
+created(){
+    this.getPostpones()
+}
 }
 </script>
 
